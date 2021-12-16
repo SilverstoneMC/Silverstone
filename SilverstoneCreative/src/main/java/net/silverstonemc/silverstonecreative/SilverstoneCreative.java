dc942c4666169c3c6c2c32f6c60d59bb018eb94f
JasonHorkles <51760372+JasonHorkles@users.noreply.github.com>
2021-12-15 21:08:22 -0700

    SilverstoneCreative

---
+++ SilverstoneCreative/src/main/java/net/silverstonemc/silverstonecreative/SilverstoneCreative.java
@@ -0,0 +1,20 @@
+package net.silverstonemc.silverstonecreative;
+
+import net.silverstonemc.silverstonecreative.commands.Plots;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings({"ConstantConditions", "unused"})
+public class SilverstoneCreative extends JavaPlugin implements Listener {
+
+    // Startup
+    @Override
+    public void onEnable() {
+        getCommand("plots?").setExecutor(new Plots());
+
+        PluginManager pluginManager = this.getServer().getPluginManager();
+
+        pluginManager.registerEvents(new StripNBT(), this);
+    }
+}
---
+++ SilverstoneCreative/src/main/java/net/silverstonemc/silverstonecreative/StripNBT.java
@@ -0,0 +1,18 @@
+package net.silverstonemc.silverstonecreative;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.inventory.InventoryInteractEvent;
+
+public class StripNBT implements Listener {
+    @EventHandler
+    public void inventory(InventoryInteractEvent event) {
+
+    }
+
+    @EventHandler
+    public void place(BlockPlaceEvent event) {
+
+    }
+}
