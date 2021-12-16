a551444f932dfa2d747c1e2d1a9255151af11534
JasonHorkles <51760372+JasonHorkles@users.noreply.github.com>
2021-12-15 22:58:50 -0700

    Remove unused import

--- SilverstoneWarnings/src/main/java/net/silverstonemc/silverstonewarnings/commands/WarnCommand.java
+++ SilverstoneWarnings/src/main/java/net/silverstonemc/silverstonewarnings/commands/WarnCommand.java
@@ -25,7 +25,6 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.Color;
-import java.util.List;
 import java.util.*;
 import java.util.stream.IntStream;
 
