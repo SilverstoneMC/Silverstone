d1c6de55c6bd0a00c7e32c342e65001bf2c5660f
JasonHorkles <51760372+JasonHorkles@users.noreply.github.com>
2022-01-09 19:25:21 -0700

    Add reload command

---
+++ SilverstoneProxy/src/main/java/net/silverstonemc/silverstoneproxy/commands/BaseCommand.java
@@ -0,0 +1,43 @@
+package net.silverstonemc.silverstoneproxy.commands;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.TabExecutor;
+import net.silverstonemc.silverstoneproxy.SilverstoneProxy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseCommand extends Command implements TabExecutor {
+
+    private final SilverstoneProxy plugin = SilverstoneProxy.getPlugin();
+
+    public BaseCommand() {
+        super("ssp", "silverstone.admin");
+    }
+
+    public void execute(CommandSender sender, String[] args) {
+        // Reload
+        if (args.length > 0) if (args[0].equalsIgnoreCase("reload")) {
+            SilverstoneProxy.config = plugin.loadConfig();
+
+            sender.sendMessage(TextComponent.fromLegacyText(ChatColor.GREEN + "SilverstoneProxy reloaded!"));
+        } else
+            sender.sendMessage(TextComponent.fromLegacyText(ChatColor.RED + "Usage: /ssp reload"));
+    }
+
+    final List<String> arguments = new ArrayList<>();
+
+    @Override
+    public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
+        if (!arguments.contains("reload")) arguments.add("reload");
+
+        List<String> result = new ArrayList<>();
+        if (args.length == 1) for (String a : arguments)
+            if (a.toLowerCase().startsWith(args[0].toLowerCase()))
+                result.add(a);
+        return result;
+    }
+}
