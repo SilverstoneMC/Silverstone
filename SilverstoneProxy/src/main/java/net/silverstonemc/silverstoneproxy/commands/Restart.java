12d3390199132b43a31daa4c00f78cb621d2a458
JasonHorkles <51760372+JasonHorkles@users.noreply.github.com>
2022-01-09 19:26:17 -0700

    Play sound on broadcast

--- SilverstoneProxy/src/main/java/net/silverstonemc/silverstoneproxy/commands/Restart.java
+++ SilverstoneProxy/src/main/java/net/silverstonemc/silverstoneproxy/commands/Restart.java
@@ -1,21 +1,28 @@
 package net.silverstonemc.silverstoneproxy.commands;
 
-import net.silverstonemc.silverstoneproxy.SilverstoneProxy;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.platform.bungeecord.BungeeAudiences;
+import net.kyori.adventure.sound.Sound;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+import net.silverstonemc.silverstoneproxy.SilverstoneProxy;
 
 public class Restart extends Command {
+
+    private final BungeeAudiences audience = SilverstoneProxy.getAdventure();
 
     public Restart() {
         super("bcnetworkrestart", "silverstone.admin");
     }
 
     public void execute(CommandSender sender, String[] args) {
-        for (ProxiedPlayer player : SilverstoneProxy.getInstance().getProxy().getPlayers())
+        for (ProxiedPlayer player : SilverstoneProxy.getPlugin().getProxy().getPlayers()) {
             player.sendMessage(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', "&c&lWARNING &b&l> &aThe network will restart soon!")));
+            audience.player(player).playSound(Sound.sound(Key.key("block.note_block.harp"), Sound.Source.MASTER, 100, 1.6f));
+        }
 
         if (!(sender instanceof ProxiedPlayer))
             sender.sendMessage(TextComponent.fromLegacyText(ChatColor.GREEN + "Restart broadcast sent!"));
