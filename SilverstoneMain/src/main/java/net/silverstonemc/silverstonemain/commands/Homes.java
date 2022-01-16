670f682ae178f151ae1e91b00d473898561da6b7
JasonHorkles <51760372+JasonHorkles@users.noreply.github.com>
2022-01-16 14:28:05 -0700

    Fix CommandException

--- SilverstoneMain/src/main/java/net/silverstonemc/silverstonemain/commands/Homes.java
+++ SilverstoneMain/src/main/java/net/silverstonemc/silverstonemain/commands/Homes.java
@@ -50,7 +50,8 @@ public class Homes implements CommandExecutor {
             }
 
             player.sendMessage(ChatColor.DARK_AQUA + "Teleporting...");
-            player.setSpectatorTarget(null);
+            if (player.getGameMode().equals(GameMode.SPECTATOR))
+                player.setSpectatorTarget(null);
             essentials.getUser(player)
                     .getAsyncTeleport()
                     .now(new Location(Bukkit.getWorld("utility"), -211.3, 38, -122.3, 107.2f, 9.5f), false, PlayerTeleportEvent.TeleportCause.COMMAND, new CompletableFuture<>());
