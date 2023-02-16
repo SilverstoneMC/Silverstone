ec9bdb8e7044448edb5025e0a445ea592a1896a2
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2023-02-15 23:21:00 -0700

    Remove redundant warning suppression

--- SilverstoneGlobal/src/main/java/net/silverstonemc/silverstoneglobal/ChatnSounds.java
+++ SilverstoneGlobal/src/main/java/net/silverstonemc/silverstoneglobal/ChatnSounds.java
@@ -22,7 +22,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
-@SuppressWarnings("UnstableApiUsage")
 public class ChatnSounds implements CommandExecutor, Listener {
     public ChatnSounds(JavaPlugin plugin) {
         this.plugin = plugin;
