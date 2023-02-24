85d0961a837fee4d6ff0048c2aa47cbfcad263b8
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2023-02-23 22:23:14 -0700

    Fix fly turning off

--- SilverstoneGlobal/src/main/java/net/silverstonemc/silverstoneglobal/events/Gamemode.java
+++ SilverstoneGlobal/src/main/java/net/silverstonemc/silverstoneglobal/events/Gamemode.java
@@ -4,10 +4,18 @@ import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class Gamemode implements Listener {
+    public Gamemode(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    private final JavaPlugin plugin;
+
     @EventHandler
     public void gamemodeChange(PlayerGameModeChangeEvent event) {
         // Night vision
@@ -15,5 +23,14 @@ public class Gamemode implements Listener {
             new PotionEffect(PotionEffectType.NIGHT_VISION, 1000000, 255, false, false, false));
         else if (event.getPlayer().getGameMode().equals(GameMode.SPECTATOR))
             event.getPlayer().removePotionEffect(PotionEffectType.NIGHT_VISION);
+
+        // Fix fly being off
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (event.getNewGameMode() == GameMode.CREATIVE) event.getPlayer().setAllowFlight(true);
+                if (event.getNewGameMode() == GameMode.SPECTATOR) event.getPlayer().setFlying(true);
+            }
+        }.runTaskLater(plugin, 10);
     }
 }
