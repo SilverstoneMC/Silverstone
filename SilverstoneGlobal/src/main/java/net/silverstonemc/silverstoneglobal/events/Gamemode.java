2b71a5510f94691842ee8a86648657bc11a4c594
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2023-02-23 21:55:47 -0700

    Remove rg bypass cuz it doesnt persist

--- SilverstoneGlobal/src/main/java/net/silverstonemc/silverstoneglobal/events/Gamemode.java
+++ SilverstoneGlobal/src/main/java/net/silverstonemc/silverstoneglobal/events/Gamemode.java
@@ -4,17 +4,10 @@ import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class Gamemode implements Listener {
-    public Gamemode(JavaPlugin plugin) {
-        this.plugin = plugin;
-    }
-
-    private final JavaPlugin plugin;
-
     @EventHandler
     public void gamemodeChange(PlayerGameModeChangeEvent event) {
         // Night vision
@@ -22,16 +15,5 @@ public class Gamemode implements Listener {
             new PotionEffect(PotionEffectType.NIGHT_VISION, 1000000, 255, false, false, false));
         else if (event.getPlayer().getGameMode().equals(GameMode.SPECTATOR))
             event.getPlayer().removePotionEffect(PotionEffectType.NIGHT_VISION);
-
-        //noinspection DataFlowIssue
-        if (plugin.getConfig().getString("server").equalsIgnoreCase("survival")) return;
-        
-        // RG bypass
-        boolean turnOn = event.getNewGameMode() == GameMode.CREATIVE || event.getNewGameMode() == GameMode.SPECTATOR;
-        boolean wasOff = event.getPlayer().getGameMode() == GameMode.SURVIVAL || event.getPlayer()
-            .getGameMode() == GameMode.ADVENTURE;
-
-        if (turnOn && wasOff) event.getPlayer().performCommand("rg bypass");
-        else if (!turnOn && !wasOff) event.getPlayer().performCommand("rg bypass");
     }
 }
