423388dfb2298e51db036e4791e856ed839ca460
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2023-10-15 19:48:07 -0600

    Remove velocity test and improve getting started command

--- SilverstoneMain/src/main/java/net/silverstonemc/silverstonemain/commands/GettingStarted.java
+++ SilverstoneMain/src/main/java/net/silverstonemc/silverstonemain/commands/GettingStarted.java
@@ -11,24 +11,23 @@ import org.jetbrains.annotations.NotNull;
 
 public class GettingStarted implements CommandExecutor {
     public boolean onCommand(CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
-        sender.sendMessage(Component.text("\nGETTING STARTED").color(TextColor.fromHexString("#18d958"))
-            .decorate(TextDecoration.BOLD));
+        sender.sendMessage(
+            Component.text("\nGETTING STARTED", TextColor.fromHexString("#18d958"), TextDecoration.BOLD));
 
-        sender.sendMessage(Component.text("-----------------").color(TextColor.fromHexString("#305abf")));
+        sender.sendMessage(Component.text("-----------------", TextColor.fromHexString("#305abf")));
 
-        sender.sendMessage(Component.text().append(
-                Component.text("Type ").color(TextColor.fromHexString("#18c1c7"))
-                    .decoration(TextDecoration.BOLD, false)).append(
-                Component.text("/warps").color(TextColor.fromHexString("#305abf"))
-                    .clickEvent(ClickEvent.suggestCommand("/warps")))
-            .append(Component.text(" to see all available warps, ").color(TextColor.fromHexString("#18c1c7")))
-            .append(Component.text("/server").color(TextColor.fromHexString("#305abf"))
-                .clickEvent(ClickEvent.suggestCommand("/server"))).append(
-                Component.text(" to see all available servers, and ")
-                    .color(TextColor.fromHexString("#18c1c7"))).append(
-                Component.text("/tips").color(TextColor.fromHexString("#305abf"))
-                    .clickEvent(ClickEvent.suggestCommand("/tips"))).append(
-                Component.text(" for tips about the network.").color(TextColor.fromHexString("#18c1c7"))));
+        sender.sendMessage(Component.text("/htp", TextColor.fromHexString("#305abf"))
+            .clickEvent(ClickEvent.suggestCommand("/htp"))
+            .append(Component.text(" - Open the How to Play menu\n", TextColor.fromHexString("#18c1c7")))
+            .append(Component.text("/warps", TextColor.fromHexString("#305abf"))
+                .clickEvent(ClickEvent.suggestCommand("/warps")))
+            .append(Component.text(" - List all available warps\n", TextColor.fromHexString("#18c1c7")))
+            .append(Component.text("/server", TextColor.fromHexString("#305abf"))
+                .clickEvent(ClickEvent.suggestCommand("/server")))
+            .append(Component.text(" - List all available servers\n", TextColor.fromHexString("#18c1c7")))
+            .append(Component.text("/tips", TextColor.fromHexString("#305abf"))
+                .clickEvent(ClickEvent.suggestCommand("/tips")))
+            .append(Component.text(" - View tips about the network", TextColor.fromHexString("#18c1c7"))));
         return true;
     }
 }
