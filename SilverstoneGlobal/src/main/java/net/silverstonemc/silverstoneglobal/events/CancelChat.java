4c9335bf815653506d7ba06394aaa9a91daeb170
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2023-11-26 15:46:26 -0700

    Remove redundant value

--- SilverstoneGlobal/src/main/java/net/silverstonemc/silverstoneglobal/events/CancelChat.java
+++ SilverstoneGlobal/src/main/java/net/silverstonemc/silverstoneglobal/events/CancelChat.java
@@ -6,7 +6,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 public class CancelChat implements Listener {
-    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = false)
+    @EventHandler(priority = EventPriority.HIGH)
     public void onChat(AsyncChatEvent event) {
         event.setCancelled(true);
     }
