e7636087dca168a7cad9912c99e7b664bcabcf48
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2023-12-01 12:04:52 -0700

    Fix formatting and add sound

--- SilverstoneProxy/src/main/java/net/silverstonemc/silverstoneproxy/commands/Restart.java
+++ SilverstoneProxy/src/main/java/net/silverstonemc/silverstoneproxy/commands/Restart.java
@@ -1,8 +1,11 @@
 package net.silverstonemc.silverstoneproxy.commands;
 
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.command.SimpleCommand;
 import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
@@ -25,10 +28,15 @@ public class Restart implements SimpleCommand {
         CommandSource sender = invocation.source();
 
         for (Player player : i.server.getAllPlayers())
-            player.sendMessage(Component.text().append(
-                Component.text("WARNING ", NamedTextColor.RED, TextDecoration.BOLD)
+            player.sendMessage(
+                Component.text().append(Component.text("WARNING ", NamedTextColor.RED, TextDecoration.BOLD))
                     .append(Component.text("> ", NamedTextColor.AQUA, TextDecoration.BOLD))
-                    .append(Component.text("The network will restart soon!", NamedTextColor.GREEN))));
+                    .append(Component.text("The network will restart soon!", NamedTextColor.GREEN)));
+
+        ByteArrayDataOutput out = ByteStreams.newDataOutput();
+        out.writeUTF("broadcastsound");
+        for (RegisteredServer servers : i.server.getAllServers())
+            servers.sendPluginMessage(SilverstoneProxy.IDENTIFIER, out.toByteArray());
 
         if (!(sender instanceof Player))
             sender.sendMessage(Component.text("Restart broadcast sent!", NamedTextColor.GREEN));
