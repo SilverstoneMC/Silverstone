e2904bd6228930b41397a7be6d5e7b45cdc54482
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2023-11-30 17:40:53 -0700

    Change max players when toggling MiniMOTD

--- SilverstoneProxy/src/main/java/net/silverstonemc/silverstoneproxy/commands/MiniMOTDControls.java
+++ SilverstoneProxy/src/main/java/net/silverstonemc/silverstoneproxy/commands/MiniMOTDControls.java
@@ -18,7 +18,7 @@ public class MiniMOTDControls implements SimpleCommand {
     public MiniMOTDControls(SilverstoneProxy instance) {
         i = instance;
     }
-    
+
     @Override
     public boolean hasPermission(final Invocation invocation) {
         return invocation.source().hasPermission("silverstone.owner");
@@ -40,14 +40,16 @@ public class MiniMOTDControls implements SimpleCommand {
             case "enable" -> {
                 editMotdFiles("/home/container/plugins/minimotd-velocity/main.conf", true);
                 editMotdFiles("/home/container/plugins/minimotd-velocity/extra-configs/survival.conf", true);
-                i.server.getCommandManager().executeAsync(i.server.getConsoleCommandSource(), "minimotd reload");
+                i.server.getCommandManager()
+                    .executeAsync(i.server.getConsoleCommandSource(), "minimotd reload");
                 sender.sendMessage(Component.text("MiniMOTD enabled.", NamedTextColor.GREEN));
             }
 
             case "disable" -> {
                 editMotdFiles("/home/container/plugins/minimotd-velocity/main.conf", false);
                 editMotdFiles("/home/container/plugins/minimotd-velocity/extra-configs/survival.conf", false);
-                i.server.getCommandManager().executeAsync(i.server.getConsoleCommandSource(), "minimotd reload");
+                i.server.getCommandManager()
+                    .executeAsync(i.server.getConsoleCommandSource(), "minimotd reload");
                 sender.sendMessage(Component.text("MiniMOTD disabled.", NamedTextColor.RED));
             }
 
@@ -65,7 +67,8 @@ public class MiniMOTDControls implements SimpleCommand {
             try (FileWriter fileWriter = new FileWriter(file, false)) {
                 for (String line : content)
                     fileWriter.write(line.replace("motd-enabled=" + !enabled, "motd-enabled=" + enabled)
-                        .replace("icon-enabled=" + !enabled, "icon-enabled=" + enabled) + "\n");
+                        .replace("icon-enabled=" + !enabled, "icon-enabled=" + enabled)
+                        .replace("max-players-enabled=" + !enabled, "max-players-enabled=" + enabled) + "\n");
             }
             
         } catch (IOException e) {
