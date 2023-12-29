93977b6a24ec383bff849583b87c9a3f2b50d7f5
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2023-12-29 10:53:01 -0700

    Add /prefixes command to GettingStarted

--- SilverstoneMain/src/main/java/net/silverstonemc/silverstonemain/commands/GettingStarted.java
+++ SilverstoneMain/src/main/java/net/silverstonemc/silverstonemain/commands/GettingStarted.java
@@ -2,6 +2,7 @@ package net.silverstonemc.silverstonemain.commands;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.command.Command;
@@ -14,23 +15,33 @@ public class GettingStarted implements CommandExecutor {
         sender.sendMessage(
             Component.text("\nGETTING STARTED", TextColor.fromHexString("#18d958"), TextDecoration.BOLD));
 
-        sender.sendMessage(Component.text("-----------------", TextColor.fromHexString("#305abf")));
+        sender.sendMessage(
+            Component.text("                     ", NamedTextColor.GRAY, TextDecoration.STRIKETHROUGH));
 
         sender.sendMessage(Component.text("/htp", TextColor.fromHexString("#305abf"))
             .clickEvent(ClickEvent.suggestCommand("/htp"))
+
             .append(Component.text(" - Open the How to Play menu\n", TextColor.fromHexString("#18c1c7")))
             .append(Component.text("/warp", TextColor.fromHexString("#305abf"))
                 .clickEvent(ClickEvent.runCommand("/warp")))
             .append(Component.text(" - List all available warps\n", TextColor.fromHexString("#18c1c7")))
+
             .append(Component.text("/server", TextColor.fromHexString("#305abf"))
                 .clickEvent(ClickEvent.runCommand("/server")))
             .append(Component.text(" - List all available servers\n", TextColor.fromHexString("#18c1c7")))
+
             .append(Component.text("/tips", TextColor.fromHexString("#305abf"))
                 .clickEvent(ClickEvent.runCommand("/tips")))
             .append(Component.text(" - View tips about the network\n", TextColor.fromHexString("#18c1c7")))
+
             .append(Component.text("/game", TextColor.fromHexString("#305abf"))
-                .clickEvent(ClickEvent.runCommand("/game"))).append(
-                Component.text(" - Get a random minigame to play\n", TextColor.fromHexString("#18c1c7"))));
+                .clickEvent(ClickEvent.runCommand("/game")))
+            .append(Component.text(" - Get a random minigame to play\n", TextColor.fromHexString("#18c1c7")))
+
+            .append(Component.text("/prefixes", TextColor.fromHexString("#305abf"))
+                .clickEvent(ClickEvent.runCommand("/prefixes"))).append(
+                Component.text(" - Display what the prefix emojis mean\n",
+                    TextColor.fromHexString("#18c1c7"))));
         return true;
     }
 }
