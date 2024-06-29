58fe9178f183b66acf075b1fc9cce32602392534
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2024-06-29 13:26:02 -0600

    Improve code

--- SilverstoneGlobal/src/main/java/net/silverstonemc/silverstoneglobal/Whitelist.java
+++ SilverstoneGlobal/src/main/java/net/silverstonemc/silverstoneglobal/Whitelist.java
@@ -3,6 +3,7 @@ package net.silverstonemc.silverstoneglobal;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
@@ -10,13 +11,12 @@ import java.util.UUID;
 public class Whitelist {
     public static void whitelist() {
         if (Bukkit.getServer().hasWhitelist()) {
-            try {
-                Bukkit.getPlayer(UUID.fromString("a28173af-f0a9-47fe-8549-19c6bccf68da")).sendActionBar(
-                    Component.text("Whitelist is on", NamedTextColor.YELLOW));
-            } catch (NullPointerException ignored) {
-            }
-
             SilverstoneGlobal.getInstance().getLogger().warning("Whitelist is on");
+
+            Player jason = Bukkit.getPlayer(UUID.fromString("a28173af-f0a9-47fe-8549-19c6bccf68da"));
+            if (jason == null) return;
+
+            jason.sendActionBar(Component.text("Whitelist is on", NamedTextColor.YELLOW));
         }
     }
 }
