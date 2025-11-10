44305dcb336c26073459bf10fdf2a1b51a451669
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2025-11-09 17:53:47 -0700

    Remove unused instance code

--- SilverstoneCreative/src/main/java/net/silverstonemc/silverstonecreative/SilverstoneCreative.java
+++ SilverstoneCreative/src/main/java/net/silverstonemc/silverstonecreative/SilverstoneCreative.java
@@ -6,16 +6,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SilverstoneCreative extends JavaPlugin implements Listener {
-    public static SilverstoneCreative getInstance() {
-        return instance;
-    }
-
-    private static SilverstoneCreative instance;
-
     @Override
     public void onEnable() {
-        instance = this;
-
         PlotAPI plotAPI = new PlotAPI();
         plotAPI.registerListener(new PlotSquaredListener());
 
